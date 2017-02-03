0cfa32d12e04e23d8fb4eddcae4ce6bb39cdc4a8
zuizuihao <sam.peng@roadshr.com>
2017-02-03 23:22:20 +0800

    fix WechatMina createwxaqrcode bug

--- wechat_mina.js
+++ wechat_mina.js
@@ -5,15 +5,15 @@ WechatMina = {}
 WechatMina.createwxaqrcode = function (app, path, width, cb) {
   wechatAuth.getToken(app, (error, access_token) => {
     HTTP.post('https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode', {
-      headers: {
-        Accept: 'application/json'
-      },
       params: {
         access_token: access_token
       },
       data: {
         path: path,
         width: width
+      },
+      npmRequestOptions: {
+        encoding: null
       }
     }, cb)
   })
